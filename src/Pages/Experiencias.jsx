e85935ad62377ea129998a40c6dea39cfe4c666f
vazvdr <azevedo.vanderson@hotmail.com>
2025-02-19 16:09:15 -0300

    Changing order of experiences

--- src/Pages/Experiencias.jsx
+++ src/Pages/Experiencias.jsx
@@ -43,6 +43,7 @@ const Experiencias = () => {
     "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg",
     "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg",
     "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original-wordmark.svg",
+    "https://www.svgrepo.com/show/435664/file-regexp.svg",
     "https://www.svgrepo.com/show/439238/nodejs.svg",
     "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nestjs/nestjs-original.svg",
     "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg",
@@ -59,34 +60,51 @@ const Experiencias = () => {
     <section id="experiencias" className="py-10 relative">
       <div className="max-w-4xl mx-auto px-10">
         <h1
-          className={`text-3xl font-bold text-center mb-8 mt-10 ${animate ? "animate-slide-in-right" : "opacity-0"}`}
+          className={`text-3xl font-bold text-center mb-8 mt-10 ${animate ? "animate-slide-in-right" : "opacity-0"
+            }`}
           style={{ fontFamily: "DoctorGlitch" }}
         >
           {t("experiencias.titulo")}
         </h1>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-          {[imagesCard1, imagesCard2].map((images, index) => (
+          {([
+            { images: imagesCard1, link: "https://01bit.tech/", cardIndex: 1 },
+            { images: imagesCard2, link: "https://www.vandersonazevedo.com.br", cardIndex: 2 },
+          ].sort((a, b) => b.images.length - a.images.length)).map((card, index) => (
             <div
               key={index}
               style={{
                 borderImage: "linear-gradient(to right, #6b21a8, #3b82f6, #6b21a8) 1",
                 animation: "borderAnimation 1s infinite",
               }}
-              className={`card rounded-lg shadow-md p-6 transform transition-transform duration-300 hover:-translate-y-2 ${animate ? (index === 0 ? "animate-slide-in-left" : "animate-slide-in-right") : "opacity-0"}`}
+              className={`card rounded-lg shadow-md p-6 transform transition-transform duration-300 hover:-translate-y-2 ${animate
+                ? index === 0
+                  ? "animate-slide-in-left"
+                  : "animate-slide-in-right"
+                : "opacity-0"
+                }`}
             >
               <h2 className="text-xl sm:text-2xl font-bold">
-                {t(`experiencias.card${index + 1}.titulo`)}
+                {t(`experiencias.card${card.cardIndex}.titulo`)}
               </h2>
-              <h3 className="text-lg">{t(`experiencias.card${index + 1}.subtitulo`)}</h3>
-              <p className="text-sm">{t(`experiencias.card${index + 1}.periodo`)}</p>
-              <p className="mt-4 text-sm">{t(`experiencias.card${index + 1}.descricao`)}</p>
+              <h3 className="text-lg">{t(`experiencias.card${card.cardIndex}.subtitulo`)}</h3>
+              <p className="text-sm">{t(`experiencias.card${card.cardIndex}.periodo`)}</p>
+              <p className="mt-4 text-sm">{t(`experiencias.card${card.cardIndex}.descricao`)}</p>
+
               <div className="flex flex-wrap gap-4 mt-4">
-                {images.map((src, imgIndex) => (
+                {card.images.map((src, imgIndex) => (
                   <img key={imgIndex} src={src} alt={`Tech ${imgIndex}`} className="w-10 h-10" />
                 ))}
               </div>
-              <a href={index === 0 ? "https://01bit.tech/" : "https://www.vandersonazevedo.com.br"} target="_blank" rel="noopener noreferrer" title="Ver Site" className="absolute bottom-4 right-4 text-white hover:text-blue-600">
+
+              <a
+                href={card.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Ver Site"
+                className="absolute bottom-4 right-4 text-white hover:text-blue-600"
+              >
                 <FaExternalLinkAlt size={20} />
               </a>
             </div>
@@ -94,6 +112,8 @@ const Experiencias = () => {
         </div>
       </div>
     </section>
+
+
   );
 };
 
