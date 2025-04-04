6498c0369bbb5b37406f62193b19b94185d3cb74
vazvdr <azevedo.vanderson@hotmail.com>
2025-04-04 15:08:15 -0300

    Sending update on section about for small screens

--- src/Pages/Sobre.jsx
+++ src/Pages/Sobre.jsx
@@ -66,22 +66,20 @@ const Sobre = () => {
   return (
     <section id="sobre-mim" className="relative">
       {/*Codigo para telas pequenas */}
-      <div className="md:hidden ld:hidden relative z-10 flex flex-col items-center md:items-start">
-        <div className="flex flex-colum max-w-[100%]">
+      <div className="md:hidden ld:hidden relative z-10 flex flex-col items-center">
+        <div className="flex flex-col items-center max-w-[100%]">
           <div
-            className={`mt-[17%] w-[45%] ml-[2%] animate-fade-in-right
-            sm:w-[50%] sm:mt-[15%] sm:mb-[4%] sm:ml-[10%] ${animate ? "animate-slide-in-left" : "opacity-0"
-              }`}
+            className={`mt-[17%] w-[90%] animate-fade-in-right sm:mt-[15%] sm:mb-[4%] ${animate ? "animate-slide-in-left" : "opacity-0"}`}
           >
-            <h1 className="text-3xl sm:text-4xl md:text-4xl lg:text-5xl font-bold leading-tight">
-              <div className="md:mt-7 pt-0 animate-moveY lg:pt-3" >
+            <h1 className="text-3xl sm:text-4xl md:text-4xl lg:text-5xl font-bold leading-tight text-center">
+              <div className="md:mt-7 pt-0 animate-moveY lg:pt-3">
                 {t("sobre.heading.greeting")}
               </div>
               <span className="text-blue-600 hover:text-blue-400 hover:translate-x-1 transition-all duration-300">
                 {t("sobre.heading.name")}
               </span>
               <br />
-              <div className="relative min-h-[80px] sm:min-h-[80px]">
+              <div className="relative min-h-[10px] sm:min-h-[40px]">
                 <span>
                   <Typewriter
                     words={["Software Engineer", "FullStack Developer", "Java Developer", "Javascript Developer"]}
@@ -95,27 +93,59 @@ const Sobre = () => {
                 </span>
               </div>
             </h1>
-            <p className="text-base sm:text-lg leading-snug">
+            <p className="text-base sm:text-lg leading-snug text-center">
               {t("sobre.description")}
             </p>
-            <div className="mt-4 flex">
+            <div className="mt-4 flex justify-center space-x-3">
               <a
                 href="#projetos"
                 className="buttons-sobre font-semibold py-2 px-2 rounded-md hover:bg-blue-600 
-                flex items-center space-x-2 
-                transition-all transform hover:scale-105 
-                whitespace-nowrap animate-pulse-custom"
+          flex items-center space-x-2 
+          transition-all transform hover:scale-105 
+          whitespace-nowrap animate-pulse-custom"
                 onClick={(e) => handleLinkClick(e, "#projetos")}
               >
                 <span>{t("sobre.projects")}</span>
                 <FaArrowDown size={20} />
+              </a>              
+              <a
+                href="/Curriculo-Vanderson.pdf"
+                download
+                className="buttons-sobre py-1 px-2 rounded-md font-semibold 
+          shadow-lg hover:bg-blue-800 hover:text-white transition-all 
+          transform hover:scale-110"
+              >
+                {t("sobre.button")}
+              </a>
+              <a
+                href="https://www.linkedin.com/in/vanderson-de-azevedo/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-blue-500 transition-all transform hover:scale-110"
+              >
+                <FaLinkedin size={32} />
+              </a>
+              <a
+                href="https://wa.me/5521967441433?text=Ol%C3%A1,%20estou%20entrando%20em%20contato%20pelo%20link%20portf%C3%B3lio%20do%20Vanderson!"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaWhatsapp size={32} className="text-xl hover:text-green-600 transform hover:scale-110" />
+              </a>
+              <a
+                href="https://github.com/vazvdr"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-gray-400 transition-all transform hover:scale-110"
+              >
+                <FaGithub size={32} />
               </a>
             </div>
+            </div>
           </div>
+
           <div
-            className={`relative w-[55%] mt-[22%] ml-[5%] mr-[5%] mt-[15%] opacity-0 animate-fade-in-left 
-  sm:w-[50%] sm:mt-[20%] sm:mr-[5%] sm:mb-[4%] 
-  ${animate ? "animate-slide-in-right opacity-100" : ""}`}
+            className={`relative w-[50%] sm:w-[50%] mt-10 animate-fade-in-left text-center ${animate ? "animate-slide-in-right opacity-100" : "opacity-0"}`}
           >
             <div className="relative p-[3px] rounded-md bg-gradient-to-r from-gray-400 via-gray-600 to-gray-700">
               <img
@@ -126,48 +156,8 @@ const Sobre = () => {
                 onContextMenu={(e) => e.preventDefault()}
               />
             </div>
-            <div className="mt-5 flex items-center justify-center md:justify-start space-x-1">
-              <a
-                href="/Curriculo-Vanderson.pdf"
-                download
-                className="buttons-sobre py-1 px-2 rounded-md font-semibold 
-                shadow-lg hover:bg-blue-800 hover:text-white transition-all 
-                transform hover:scale-110 "
-              >
-                {t("sobre.button")}
-              </a>
-              <a
-                href="https://www.linkedin.com/in/vanderson-de-azevedo/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-blue-500 hover:translate-y-120px transition-all transform hover:scale-110"
-              >
-                <FaLinkedin size={32} />
-                <span className="sr-only">{t("sobre.linkedin")}</span>
-              </a>
-              <a
-                href="https://wa.me/5521967441433?text=Ol%C3%A1,%20estou%20entrando%20em%20contato%20pelo%20link%20portf%C3%B3lio%20do%20Vanderson!"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaWhatsapp size={32} className="text-xl hover:text-green-600 transform hover:scale-110" />
-              </a>
-              <span className="absolute left-1/2 -translate-x-1/2 bottom-8 bg-black text-white text-sm p-1 rounded-md opacity-0 group-hover:opacity-100 transition-opacity">
-                WhatsApp
-              </span>
-              <a
-                href="https://github.com/vazvdr"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-gray-400 transition-all transform hover:scale-110"
-              >
-                <FaGithub size={32} />
-                <span className="sr-only">{t("sobre.github")}</span>
-              </a>
-            </div>
           </div>
-        </div>
-      </div>
+        </div>      
 
       {/*Codigo para telas medias e grandes */}
 
