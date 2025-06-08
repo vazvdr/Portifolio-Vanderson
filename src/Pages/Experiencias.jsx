11bdeec7bc9bfef84ed364ad707fd770fef9af64
vazvdr <azevedo.vanderson@hotmail.com>
2025-06-08 12:55:49 -0300

    Sending feature: updating section experiencias

--- src/Pages/Experiencias.jsx
+++ src/Pages/Experiencias.jsx
@@ -39,16 +39,16 @@ const Experiencias = () => {
   ];
 
   const imagesCard2 = [
-  "https://www.svgrepo.com/show/452228/html-5.svg",
-  "https://www.svgrepo.com/show/349330/css3.svg",
-  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
-  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
-  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
-  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
-  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitest/vitest-original.svg",
-  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
-  "https://www.svgrepo.com/show/373624/git2.svg",
-  "https://www.svgrepo.com/show/475654/github-color.svg",
+    "https://www.svgrepo.com/show/452228/html-5.svg",
+    "https://www.svgrepo.com/show/349330/css3.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitest/vitest-original.svg",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+    "https://www.svgrepo.com/show/373624/git2.svg",
+    "https://www.svgrepo.com/show/475654/github-color.svg",
     "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
     "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg",
     "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg",
@@ -78,45 +78,74 @@ const Experiencias = () => {
         </h1>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-          {([
+          {[
             { images: imagesCard1, link: "https://01bit.tech/", cardIndex: 1 },
             { images: imagesCard2, cardIndex: 2 },
-          ].sort((a, b) => b.images.length - a.images.length)).map((card, index) => (
-            <div
-              key={index}
-              className={`card rounded-lg shadow-md p-6 transform transition-transform duration-300 hover:-translate-y-2 ${animate
-                ? index === 0
-                  ? "animate-slide-in-left"
-                  : "animate-slide-in-right"
-                : "opacity-0"
-                }`}
-            >
-              <h2 className="text-xl sm:text-2xl font-bold">
-                {t(`experiencias.card${card.cardIndex}.titulo`)}
-              </h2>
-              <h3 className="text-lg">{t(`experiencias.card${card.cardIndex}.subtitulo`)}</h3>
-              <p className="text-sm">{t(`experiencias.card${card.cardIndex}.periodo`)}</p>
-              <p className="mt-4 text-sm">{t(`experiencias.card${card.cardIndex}.descricao`)}</p>
+          ]
+            .sort((a, b) => b.images.length - a.images.length)
+            .map((card, index) => (
+              <div
+                key={index}
+                className={`card rounded-lg shadow-md p-6 transform transition-transform duration-300 hover:-translate-y-2 ${animate
+                    ? index === 0
+                      ? "animate-slide-in-left"
+                      : "animate-slide-in-right"
+                    : "opacity-0"
+                  }`}
+              >
+                <h2 className="text-xl sm:text-2xl font-bold">
+                  {t(`experiencias.card${card.cardIndex}.titulo`)}
+                </h2>
+                <h3 className="text-lg">
+                  {t(`experiencias.card${card.cardIndex}.subtitulo`)}
+                </h3>
+                <p className="text-sm">
+                  {t(`experiencias.card${card.cardIndex}.periodo`)}
+                </p>
 
-              <div className="flex flex-wrap gap-4 mt-4">
-                {card.images.map((src, imgIndex) => (
-                  <img key={imgIndex} src={src} alt={`Tech ${imgIndex}`} className="w-10 h-10" />
-                ))}
+                {/* Renderização da descrição */}
+                {Array.isArray(
+                  t(`experiencias.card${card.cardIndex}.descricao`, {
+                    returnObjects: true,
+                  })
+                ) ? (
+                  <ul className="mt-4 list-disc list-inside text-sm space-y-1">
+                    {t(`experiencias.card${card.cardIndex}.descricao`, {
+                      returnObjects: true,
+                    }).map((item, i) => (
+                      <li key={i}>{item}</li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p className="mt-4 text-sm">
+                    {t(`experiencias.card${card.cardIndex}.descricao`)}
+                  </p>
+                )}
+
+                <div className="flex flex-wrap gap-4 mt-4">
+                  {card.images.map((src, imgIndex) => (
+                    <img
+                      key={imgIndex}
+                      src={src}
+                      alt={`Tech ${imgIndex}`}
+                      className="w-10 h-10"
+                    />
+                  ))}
+                </div>
+
+                {card.link && (
+                  <a
+                    href={card.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="Ver Site"
+                    className="absolute bottom-4 right-4 hover:text-blue-600"
+                  >
+                    <FaExternalLinkAlt size={20} />
+                  </a>
+                )}
               </div>
-
-              {card.link && (
-                <a
-                  href={card.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  title="Ver Site"
-                  className="absolute bottom-4 right-4 hover:text-blue-600"
-                >
-                  <FaExternalLinkAlt size={20} />
-                </a>
-              )}
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </section>
