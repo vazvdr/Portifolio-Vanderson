0e8260269d5da92efe7beaa7a4ba0d7a2f83a1a1
vazvdr <azevedo.vanderson@hotmail.com>
2025-06-17 12:49:35 -0300

    Revert "Sending feature: new contact info and form"
    This reverts commit bdb028b755123aff696020b9dc5b5bd99b67be8c.

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -7,15 +7,8 @@ const Contact = () => {
   const { t } = useTranslation();
   const [alertMessage, setAlertMessage] = useState(null);
   const [errors, setErrors] = useState({});
-  const [isMdOrLarger, setIsMdOrLarger] = useState(false);
-
-  useEffect(() => {
-    const checkScreenSize = () => setIsMdOrLarger(window.innerWidth >= 768);
-    checkScreenSize();
-    window.addEventListener("resize", checkScreenSize);
-    return () => window.removeEventListener("resize", checkScreenSize);
-  }, []);
-
+  const controls = useAnimation();
+  const sectionRef = useRef(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -60,6 +53,29 @@ const Contact = () => {
 
   const closeAlert = () => setAlertMessage(null);
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          controls.start({ opacity: 1, scale: 1, transition: { duration: 1 } });
+        } else {
+          controls.start({ opacity: 1, scale: 1 });
+        }
+      },
+      { threshold: 0.5, once: true }
+    );
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current);
+      }
+    };
+  }, [controls]);
+
   return (
     <section className="flex flex-col items-center justify-center mt-[1.5%] px-4">
       {alertMessage && (
@@ -76,66 +92,20 @@ const Contact = () => {
         </div>
       )}
 
-      <div id="contato" className="w-full mb-[12%] sm:mb-[12%] md:mb-[6%] lg:mb-[2%] flex flex-col items-center md:w-[98%] md:h-[600px] lg:w-[80%] lg:h-[600px]" >
+      <div id="contato" className="w-full mb-[2%] flex flex-col items-center md:w-[90%] lg:w-[70%]" >
         <h1 className="text-center text-3xl mb-6 mt-[8%]" style={{ fontFamily: "DoctorGlitch" }}>
           {t("contato.contactInfo")}
         </h1>
-        <div className="w-full flex flex-col md:flex-row md:mb-[5%]">
-          {isMdOrLarger ? (
-            <motion.div
-              initial={{ opacity: 0, x: -100 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.8, ease: "easeOut" }}
-              viewport={{ once: false }}
-              className="formulario md:py-[5%] md:px-[1%] lg:px-3 md:w-1/2 lg:py-[5.3%] lg:w-1/2 md:rounded-l-lg flex flex-col justify-center"
+
+        <div className="w-full flex flex-col md:flex-row md:space-x-0 md:mb-[5%] ">
+          {/* Div com as informações de contato */}
+          <motion.div
+            ref={sectionRef}
+            className="formulario w-full px-2 py-2 md:py-[7.7%] md:px-[2%] md:py-16 lg:px-8 lg:py-[6.2%] lg:w-1/2 rounded-lg md:rounded-l-lg shadow-lg border border-purple-800 h-full flex-1 border-gradient"
+            animate={controls}
+            initial={{ opacity: 0, scale: 0.5 }}
             >
-              <div className="w-full space-y-6 text-lg">
-                <div
-                  className="flex flex-col items-start cursor-pointer contact-info bg-clip-border hover:bg-black hover:text-white hover:scale-105 transition-all duration-300 ease-in-out"
-                  onClick={() => window.open("https://www.linkedin.com/in/vanderson-de-azevedo/", "_blank")}
-                >
-
-                  <div className="flex items-center space-x-3">
-                    <FaLinkedin className="text-3xl" />
-                    <span>Linkedin</span>
-                  </div>
-                  <p className="pl-[42px]">Vanderson de Azevedo</p>
-                </div>
-
-                <div
-                  className="flex flex-col items-start cursor-pointer contact-info bg-clip-border hover:bg-black hover:text-white hover:scale-105 transition-all duration-300 ease-in-out"
-                  onClick={() => window.open("mailto:vanderson.azevedo.rocha@gmail.com")}
-                >
-                  <div className="flex items-center space-x-3">
-                    <FaEnvelope className="text-3xl" />
-                    <span>Email</span>
-                  </div>
-                  <p className="pl-[42px]">vanderson.azevedo.rocha@gmail.com</p>
-                </div>
-
-                <div
-                  className="flex flex-col items-start cursor-pointer contact-info hover:bg-black hover:text-white hover:scale-105 transition-all duration-300 ease-in-out"
-                  onClick={() => window.open("https://wa.me/5521967441433?text=Ol%C3%A1,%20estou%20entrando%20em%20contato%20pelo%20link%20portf%C3%B3lio%20do%20Vanderson!", "_blank")}
-                >
-                  <div className="flex items-start space-x-3">
-                    <FaWhatsapp className="text-3xl" />
-                    <span>WhatsApp</span>
-                  </div>
-                  <p className="pl-[42px]">+55 (21) 96744-1433</p>
-                </div>
-
-                <div className="flex flex-col items-start cursor-pointer contact-info bg-clip-border hover:bg-black hover:text-white hover:scale-105 transition-all duration-300 ease-in-out"
-                >
-                  <div className="flex items-center space-x-3">
-                    <FaMapMarkerAlt className="text-3xl" />
-                    <span>{t("contato.adress")}</span>
-                  </div>
-                  <p className="pl-[42px]">Rio de Janeiro, Brasil</p>
-                </div>
-              </div>
-            </motion.div>
-          ) : (
-            <div className="formulario w-full px-4 py-4 space-y-6 text-lg">
+            <div className="w-full rounded-lg shadow-lg flex flex-col justify-center space-y-6 text-lg">
               <div
                 className="flex flex-col items-start cursor-pointer contact-info bg-clip-border hover:bg-black hover:text-white hover:scale-105 transition-all duration-300 ease-in-out"
                 onClick={() => window.open("https://www.linkedin.com/in/vanderson-de-azevedo/", "_blank")}
@@ -179,140 +149,74 @@ const Contact = () => {
                 <p className="pl-[42px]">Rio de Janeiro, Brasil</p>
               </div>
             </div>
-          )}
+          </motion.div>
 
-          {isMdOrLarger ? (
-            <motion.form
-              initial={{ opacity: 0, x: 100 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.8, ease: "easeOut" }}
-              viewport={{ once: false }}
-              className="formulario h-full flex-1 md:px-[2%] md:py-[4.8%] lg:py-[3.6%]"
-              onSubmit={handleSubmit}
-            >
-              <form className="space-y-2" onSubmit={handleSubmit}>
-                {/* Nome */}
-                <div>
-                  <label htmlFor="name" className="block text-sm font-medium mb-2">
-                    {t("contato.namelabel")}
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    placeholder={errors.name ? errors.name : t("contato.nameplaceholder")}
-                    className={`w-full px-4 py-2 formulario hover:scale-105 transition-all 500
-                  ${errors.name ? "border-red-500 placeholder-red-500" : ""
-                      }`}
-                  />
-                </div>
+          {/* Div com o formulário */}
+          <motion.div
+            ref={sectionRef}
+            className="formulario w-full p-9 shadow-lg h-full flex-1 md:mb-[1%] md:py-[4.8%] lg:py-[3.8%]"
+            animate={controls}
+            initial={{ opacity: 0, scale: 0.5 }}
+          >
+            <form className="space-y-2 md:py-[2%]" onSubmit={handleSubmit}>
+              {/* Nome */}
+              <div>
+                <label htmlFor="name" className="block text-sm font-medium mb-2">
+                  {t("contato.namelabel")}
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder={errors.name ? errors.name : t("contato.nameplaceholder")}                  
+                  className={`w-full px-4 py-2 rounded-lg formulario 
+                  focus:border-transparent ${errors.name ? "border-red-500 placeholder-red-500" : ""
+                    }`}
+                />
+              </div>
 
-                {/* Email */}
-                <div>
-                  <label htmlFor="email" className="block text-sm font-medium mb-2">
-                    {t("contato.emaillabel")}
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder={errors.email ? errors.email : t("contato.emailplaceholder")}
-                    className={`w-full px-4 py-2 formulario hover:scale-105 transition-all 500
-                  ${errors.email ? "border-red-500 placeholder-red-500" : ""
-                      }`}
-                  />
-                </div>
+              {/* Email */}
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium mb-2">
+                  {t("contato.emaillabel")}
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder={errors.email ? errors.email : t("contato.emailplaceholder")}
+                  className={`w-full px-4 py-2 rounded-lg border-b border-l formulario focus:outline-none focus:ring-2 focus:border-transparent ${errors.email ? "border-red-500 placeholder-red-500" : ""
+                    }`}
+                />
+              </div>
 
-                {/* Mensagem */}
-                <div>
-                  <label htmlFor="message" className="block text-sm font-medium mb-2">
-                    {t("contato.messagelabel")}
-                  </label>
-                  <textarea
-                    id="message"
-                    name="message"
-                    placeholder={errors.message ? errors.message : t("contato.messageplaceholder")}
-                    rows="3"
-                    className={`w-full px-4 py-2 formulario hover:scale-105 transition-all 500
-                  ${errors.message ? "border-red-500 placeholder-red-500" : ""
-                      }`}
-                  ></textarea>
-                </div>
+              {/* Mensagem */}
+              <div>
+                <label htmlFor="message" className="block text-sm font-medium mb-2">
+                  {t("contato.messagelabel")}
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  placeholder={errors.message ? errors.message : t("contato.messageplaceholder")}
+                  rows="3"
+                  className={`w-full px-4 py-2 rounded-lg border-b border-l formulario focus:outline-none focus:ring-2 focus:border-transparent ${errors.message ? "border-red-500 placeholder-red-500" : ""
+                    }`}
+                ></textarea>
+              </div>
 
-                {/* Botão de Envio */}
-                <button
-                  type="submit"
-                  className="formulario w-full py-3 px-6 rounded-lg bg-transparent 
-                hover:bg-black hover:text-white hover:scale-105 transition-all 500
-                font-bold"
-                >
-                  {t("contato.submitbutton")}
-                </button>
-              </form>
-              </motion.form>
-          ) : (
-            <form className="formulario p-6 h-full space-y-2" onSubmit={handleSubmit}>
-                {/* Nome */}
-                <div>
-                  <label htmlFor="name" className="block text-sm font-medium mb-2">
-                    {t("contato.namelabel")}
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    placeholder={errors.name ? errors.name : t("contato.nameplaceholder")}
-                    className={`w-full px-4 py-2 formulario hover:scale-105 transition-all 500
-                  ${errors.name ? "border-red-500 placeholder-red-500" : ""
-                      }`}
-                  />
-                </div>
-
-                {/* Email */}
-                <div>
-                  <label htmlFor="email" className="block text-sm font-medium mb-2">
-                    {t("contato.emaillabel")}
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder={errors.email ? errors.email : t("contato.emailplaceholder")}
-                    className={`w-full px-4 py-2 formulario hover:scale-105 transition-all 500
-                  ${errors.email ? "border-red-500 placeholder-red-500" : ""
-                      }`}
-                  />
-                </div>
-
-                {/* Mensagem */}
-                <div>
-                  <label htmlFor="message" className="block text-sm font-medium mb-2">
-                    {t("contato.messagelabel")}
-                  </label>
-                  <textarea
-                    id="message"
-                    name="message"
-                    placeholder={errors.message ? errors.message : t("contato.messageplaceholder")}
-                    rows="3"
-                    className={`w-full px-4 py-2 formulario hover:scale-105 transition-all 500
-                  ${errors.message ? "border-red-500 placeholder-red-500" : ""
-                      }`}
-                  ></textarea>
-                </div>
-
-                {/* Botão de Envio */}
-                <button
-                  type="submit"
-                  className="formulario w-full py-3 px-6 rounded-lg bg-transparent 
-                hover:bg-black hover:text-white hover:scale-105 transition-all 500
-                font-bold"
-                >
-                  {t("contato.submitbutton")}
-                </button>
-              </form>
-          )}
-
-            </div>
+              {/* Botão de Envio */}
+              <button
+                type="submit"
+                className="formulario w-full py-3 px-6 rounded-lg bg-transparent 
+                border border-purple-800 hover:bg-black hover:text-white
+                font-bold focus:outline-none focus:ring-2 focus:ring-offset-2"
+              >
+                {t("contato.submitbutton")}
+              </button>
+            </form>
+          </motion.div>
+        </div>
       </div>
     </section>
   );
