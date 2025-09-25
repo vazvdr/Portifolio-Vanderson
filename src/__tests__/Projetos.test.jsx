2ce104d42033ff4330fefbc23dbe3f1741f87606
vazvdr <azevedo.vanderson@hotmail.com>
2025-09-25 10:28:05 -0300

    Sending new import on projetos test

--- src/__tests__/Projetos.test.jsx
+++ src/__tests__/Projetos.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import Projetos from '../pages/Projetos';
+import Projetos from '../Pages/Projetos';
 import { I18nextProvider } from 'react-i18next';
 import i18n from '../i18n'
 
