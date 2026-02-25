bb9a389ba1367b13db8ee490c21820183eb0acd8
vazvdr <azevedo.vanderson@hotmail.com>
2026-02-25 15:12:15 -0300

    Corrigindo o bug de case sensitive

---
+++ src/pages-temp/Formacao.jsx
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import Estacio from "../assets/Estacio.png";
+import Docker from "../assets/Docker.jpg";
+import Tailwind from "../assets/TailwindCSS.jpg";
+import Git from "../assets/GitHub.jpg";
+import GitLab from '../assets/GitLab.jpg';
+import Linux from "../assets/Linux.jpg";
+import English from "../assets/Proficient-English.jpg";
+import Spring from '../assets/Spring2025.jpg';
+import SpringMicrosservices from '../assets/SpringMicrosservices2025.jpg';
+import SpringRabbitMQ from '../assets/SpringRabbitMQ.jpg';
+import ReactCourse from '../assets/React.jpg';
+import Angular from '../assets/Angular.jpg';
+
+const Formacao = () => {
+  const { t } = useTranslation();
+  const [animate, setAnimate] = useState(window.innerWidth >= 768);
+  const [showAll, setShowAll] = useState(false);
+  const [visibleCards, setVisibleCards] = useState(
+    window.innerWidth >= 1024 ? 3 : 2
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setVisibleCards(window.innerWidth >= 1024 ? 3 : 2);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    const handleAnimation = () => {
+      if (window.innerWidth < 768) {
+        setAnimate(true);
+        return;
+      }
+
+      const section = document.getElementById("formacao");
+      if (!section) return;
+
+      const observer = new IntersectionObserver(
+        ([entry]) => {
+          if (entry.isIntersecting) {
+            setAnimate(false);
+            setTimeout(() => setAnimate(true), 100);
+          }
+        },
+        { threshold: 0.3 }
+      );
+
+      observer.observe(section);
+      return () => observer.unobserve(section);
+    };
+
+    handleAnimation();
+    window.addEventListener("resize", handleAnimation);
+
+    return () => window.removeEventListener("resize", handleAnimation);
+  }, []);
+
+  const cursos = [
+    { img: Estacio, titulo: "formacao.cardCollege.titulo", instituicao: "formacao.cardCollege.subtitulo", periodo: "formacao.cardCollege.periodo", descricao: "formacao.cardCollege.descricao" },
+    { img: English, titulo: "formacao.cardEnglish.titulo", instituicao: "formacao.cardEnglish.subtitulo", periodo: "formacao.cardEnglish.periodo", descricao: "formacao.cardEnglish.descricao" },
+    { img: Angular, titulo: "formacao.cardAngular.titulo", instituicao: "formacao.cardAngular.subtitulo", periodo: "formacao.cardAngular.periodo", descricao: "formacao.cardAngular.descricao" },
+    { img: GitLab, titulo: "formacao.cardGitLab.titulo", instituicao: "formacao.cardGitLab.subtitulo", periodo: "formacao.cardGitLab.periodo", descricao: "formacao.cardGitLab.descricao" },
+    { img: SpringRabbitMQ, titulo: "formacao.cardSpringRabbitMQ.titulo", instituicao: "formacao.cardSpringRabbitMQ.subtitulo", periodo: "formacao.cardSpringRabbitMQ.periodo", descricao: "formacao.cardSpringRabbitMQ.descricao" },
+    { img: SpringMicrosservices, titulo: "formacao.cardSpringMicrosservices.titulo", instituicao: "formacao.cardSpringMicrosservices.subtitulo", periodo: "formacao.cardSpringMicrosservices.periodo", descricao: "formacao.cardSpringMicrosservices.descricao" },
+    { img: ReactCourse, titulo: "formacao.cardReact.titulo", instituicao: "formacao.cardReact.subtitulo", periodo: "formacao.cardReact.periodo", descricao: "formacao.cardReact.descricao" },
+    { img: Spring, titulo: "formacao.cardSpring.titulo", instituicao: "formacao.cardSpring.subtitulo", periodo: "formacao.cardSpring.periodo", descricao: "formacao.cardSpring.descricao" },
+    { img: Docker, titulo: "formacao.cardDocker.titulo", instituicao: "formacao.cardDocker.subtitulo", periodo: "formacao.cardDocker.periodo", descricao: "formacao.cardDocker.descricao" },
+    { img: Tailwind, titulo: "formacao.cardTailwind.titulo", instituicao: "formacao.cardTailwind.subtitulo", periodo: "formacao.cardTailwind.periodo", descricao: "formacao.cardTailwind.descricao" },
+    { img: Git, titulo: "formacao.cardGit.titulo", instituicao: "formacao.cardGit.subtitulo", periodo: "formacao.cardGit.periodo", descricao: "formacao.cardGit.descricao" },
+    { img: Linux, titulo: "formacao.cardLinux.titulo", instituicao: "formacao.cardLinux.subtitulo", periodo: "formacao.cardLinux.periodo", descricao: "formacao.cardLinux.descricao" },
+  ];
+
+  return (
+    <section
+      id="formacao"
+      className={`relative py-16 w-full flex flex-col items-center transition-all duration-1000 ${animate ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+        }`}
+    >
+      <h1
+        className="text-2xl md:text-3xl md:font-bold  mb-8 mt-20 animate-slide-left"
+        style={{ fontFamily: "DoctorGlitch" }}
+      >
+        {t("formacao.titulo")}
+      </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 gap-x-0">
+        {(showAll ? cursos : cursos.slice(0, visibleCards)).map((curso, index) => (
+          <div
+            key={index}
+            className="card p-6 w-[90%] mx-auto rounded-lg shadow-lg hover:scale-105 transition-all duration-300"
+          >
+            <img
+              src={curso.img}
+              alt={t(curso.titulo)}
+              className="w-[100%] object-contain"
+            />
+            <h2 className="text-xl font-semibold">{t(curso.titulo)}</h2>
+            <p>{t(curso.instituicao)}</p>
+            <p className="mb-2">{t(curso.periodo)}</p>
+            <ul className="list-disc list-inside text-sm space-y-1">
+              {t(curso.descricao, { returnObjects: true }).map((item, i) => (
+                <li key={i} className="block">
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+
+      {cursos.length > visibleCards && (
+        <button
+          onClick={() => setShowAll(!showAll)}
+          className="card mt-6 px-4 py-2 w-[90%] rounded-lg font-bold text-3x1 transition-all hover:scale-105"
+        >
+          {showAll ? t("formacao.verMenos") : t("formacao.verMais")}
+        </button>
+      )}
+    </section>
+  );
+};
+
+export default Formacao;
---
+++ src/pages-temp/Header.jsx
@@ -0,0 +1,354 @@
+import React, { useState, useRef, useEffect } from "react";
+import { SiAlienware } from "react-icons/si";
+import { useTranslation } from "react-i18next";
+import Flag from "react-world-flags";
+import { ArrowUpCircle, Sun, Moon } from 'react-feather';
+import { useTheme } from '../ThemeProvider'
+import { HoverBorderGradient } from "../components/ui/hover-border-gradient";
+
+const Header = () => {
+  const { t, i18n } = useTranslation();
+  const [showLangOptionsMobile, setShowLangOptionsMobile] = useState(false);
+  const [showLangOptionsDesktop, setShowLangOptionsDesktop] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [showScrollButton, setShowScrollButton] = useState(false);
+  const { theme, toggleTheme } = useTheme();
+  const menuRef = useRef(null);
+  const langRefMobile = useRef(null);
+  const langRefDesktop = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        langRefMobile.current &&
+        !langRefMobile.current.contains(event.target)
+      ) {
+        setShowLangOptionsMobile(false);
+      }
+
+      if (
+        langRefDesktop.current &&
+        !langRefDesktop.current.contains(event.target)
+      ) {
+        setShowLangOptionsDesktop(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  // Função para fechar o menu ao clicar fora
+  const handleClickOutside = (event) => {
+    if (!event.target.closest('.menu') && !event.target.closest('.menu-button')) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+    return () => document.removeEventListener('click', handleClickOutside);
+  }, []);
+
+  // Função para aparecer o botão que sobe para o topo
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setShowScrollButton(true);
+      } else {
+        setShowScrollButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  // Função para traduzir
+  const changeLanguage = (lng) => {
+    const value = lng === "pt" ? "pt-BR" : "en";
+    i18n.changeLanguage(value);
+    localStorage.setItem("i18nextLng", value);
+  };
+
+  // Suave rolagem ao clicar em um link do menu
+  const handleLinkClick = (e, target) => {
+    e.preventDefault();
+    const element = document.querySelector(target);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+    setIsOpen(false);
+  };
+
+  const sections = ["#sobre-mim", "#formacao", "#experiencias", "#tecnologias", "#projetos", "#contato"];
+  const [selectedSection, setSelectedSection] = useState(null);
+
+  // Função para tratar clique no menu
+  const handleLinkClickSelect = (e, section) => {
+    e.preventDefault();
+    setSelectedSection(section);
+    document.querySelector(section)?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  // Função para monitorar o scroll e atualizar o item selecionado
+  useEffect(() => {
+    const handleScroll = () => {
+      let currentSection = null;
+
+      sections.forEach((section) => {
+        const element = document.querySelector(section);
+        if (element) {
+          const rect = element.getBoundingClientRect();
+          if (rect.top <= 150 && rect.bottom >= 150) {
+            currentSection = section;
+          }
+        }
+      });
+
+      if (currentSection) {
+        setSelectedSection(currentSection);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
+  return (
+    <>
+      <header className="fixed top-0 left-0 w-full z-50 h-[70px] backdrop-blur-lg flex justify-between items-center">
+        <div className="header-line absolute inset-x-0 bottom-0 h-[1.5px]"></div>
+        <div className="container mx-auto flex items-center justify-between">
+          <a href="/" className="inline-flex items-center space-x-1">
+            <SiAlienware data-testid="alien-icon" size={50} className="alien transform hover:scale-110" />
+            <span className="w-[30px] h-38 font-bold text-lg  text-center" style={{ fontFamily: "DoctorGlitch" }}>
+              {t("header.developer")}
+            </span>
+          </a>
+          <div className="lg:hidden flex items-center justify-between">
+
+            <div className="relative mt-2" ref={langRefMobile}>
+              <button
+                onClick={() => setShowLangOptionsMobile(prev => !prev)}
+                className="w-6 h-6 bg-secondary-01 shadow-lg transform hover:scale-110 transition duration-300 flex items-center justify-center rounded-full"
+              >
+                <Flag
+                  code={i18n.language?.startsWith("pt") ? "BR" : "US"}
+                  alt="Idioma atual"
+                  className="w-full h-full object-cover rounded-full"
+                />
+              </button>
+              {showLangOptionsMobile && (
+                <div className="absolute top-0 right-0 min-w-[90px] cursor-pointer bg-black rounded-md shadow-lg px-4 py-2 flex flex-row gap-2 z-50">
+                  <button onClick={() => { changeLanguage("pt"); setShowLangOptionsMobile(false); }}>
+                    <Flag code="BR" className="w-6 h-6 object-cover rounded-full" alt="Português" />
+                  </button>
+                  <button onClick={() => { changeLanguage("en"); setShowLangOptionsMobile(false); }}>
+                    <Flag code="US" className="w-6 h-6 object-cover rounded-full cursor-pointer" alt="Inglês" />
+                  </button>
+                </div>
+              )}
+            </div>
+
+            {/* Botão do menu hambúrguer */}
+            <button
+              className="menu-button mr-2 ml-3"
+              onClick={toggleMenu}
+            >
+              {isOpen ? '✖' : '☰'}
+            </button>
+            <button
+              onClick={toggleTheme}
+              className="p-2 mt-[3%] rounded-full transition duration-300 flex items-center justify-center w-12 h-12 border-none"
+            >
+              {theme === 'dark' ? <Sun size={22} className="text-yellow-400 hover:scale-110" /> : <Moon size={22} className="text-blue-600 hover:scale-110" />}
+            </button>
+
+            {/* Menu aberto para telas menores */}
+            <div
+              ref={menuRef}
+              className={`menu-hamburguer absolute top-[70px] right-0 w-[100%] max-h-[300px]
+    flex flex-col items-center py-4 mb-4 z-50 shadow-lg border-b border-gray-600 
+    lg:hidden overflow-hidden transition-all duration-1000 ease-in-out ${isOpen ? "h-auto opacity-100" : "h-0 opacity-0"
+                }`}
+            >
+              <ul className="flex flex-col space-y-4">
+                <li>
+                  <a
+                    href="#sobre-mim"
+                    className={`hover:text-gray-600 font-semibold transition-all duration-300 
+            ${selectedSection === "#sobre-mim" ? "border-b-2 border-gray-600 pb-1" : "border-b-2 border-transparent"}`}
+                    onClick={(e) => handleLinkClickSelect(e, "#sobre-mim")}
+                  >
+                    {t("header.menu.about")}
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#formacao"
+                    className={`hover:text-gray-600 font-semibold transition-all duration-300 
+            ${selectedSection === "#formacao" ? "border-b-2 border-gray-600 pb-1" : "border-b-2 border-transparent"}`}
+                    onClick={(e) => handleLinkClickSelect(e, "#formacao")}
+                  >
+                    {t("header.menu.education")}
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#experiencias"
+                    className={`hover:text-gray-600 font-semibold transition-all duration-300 
+            ${selectedSection === "#experiencias" ? "border-b-2 border-gray-600 pb-1" : "border-b-2 border-transparent"}`}
+                    onClick={(e) => handleLinkClickSelect(e, "#experiencias")}
+                  >
+                    {t("header.menu.experiences")}
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#tecnologias"
+                    className={`hover:text-gray-600 font-semibold transition-all duration-300 
+            ${selectedSection === "#tecnologias" ? "border-b-2 border-gray-600 pb-1" : "border-b-2 border-transparent"}`}
+                    onClick={(e) => handleLinkClickSelect(e, "#tecnologias")}
+                  >
+                    {t("header.menu.technologies")}
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#projetos"
+                    className={`hover:text-gray-600 font-semibold transition-all duration-300 
+            ${selectedSection === "#projetos" ? "border-b-2 border-gray-600 pb-1" : "border-b-2 border-transparent"}`}
+                    onClick={(e) => handleLinkClickSelect(e, "#projetos")}
+                  >
+                    {t("header.menu.projects")}
+                  </a>
+                </li>
+                <li>
+                  <HoverBorderGradient
+                    as="button"
+                    onClick={(e) => handleLinkClickSelect(e, "#contato")}
+                    containerClassName={`transform hover:scale-110 transition-all duration-500 ease-in-out ${selectedSection === "#contato" ? "border-gray-600" : ""
+                      }`}
+                    className="contact-button font-semibold"
+                  >
+                    {t("header.menu.contact")}
+                  </HoverBorderGradient>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        {/* Menu para telas maiores (lg e acima) */}
+        <nav className="hidden lg:flex items-center space-x-10 mr-[2%] animate-fadeInRight delay-400">
+          <ul className="flex items-center lg:space-x-5">
+            {/* Botões de bandeiras (desktop) */}
+            <div className="relative" ref={langRefDesktop}>
+              <button
+                onClick={() => setShowLangOptionsDesktop(prev => !prev)}
+                className="w-6 h-6 bg-secondary-01 shadow-lg transform hover:scale-110 transition duration-300 flex items-center justify-center rounded-full"
+              >
+                <Flag
+                  code={i18n.language?.startsWith("pt") ? "BR" : "US"}
+                  alt="Idioma atual"
+                  className="w-full h-full object-cover rounded-full"
+                />
+              </button>
+              {showLangOptionsDesktop && (
+                <div className="absolute top-0 right-0 min-w-[90px] bg-black rounded-md shadow-lg px-4 py-2 flex flex-row gap-3 z-50">
+                  <button onClick={() => { changeLanguage("pt"); setShowLangOptionsDesktop(false); }}>
+                    <Flag code="BR" className="w-6 h-6 object-cover rounded-full" alt="Português" />
+                  </button>
+                  <button onClick={() => { changeLanguage("en"); setShowLangOptionsDesktop(false); }}>
+                    <Flag code="US" className="w-6 h-6 object-cover rounded-full" alt="Inglês" />
+                  </button>
+                </div>
+              )}
+            </div>
+
+            {/* Itens do menu */}
+            {[
+              { id: "#sobre-mim", label: t("header.menu.about") },
+              { id: "#formacao", label: t("header.menu.education") },
+              { id: "#experiencias", label: t("header.menu.experiences") },
+              { id: "#tecnologias", label: t("header.menu.technologies") },
+              { id: "#projetos", label: t("header.menu.projects") },
+            ].map((item) => (
+              <li key={item.id}>
+                <a
+                  href={item.id}
+                  className={`buttons-menu font-semibold whitespace-nowrap transition-all duration-300 
+            ${selectedSection === item.id
+                      ? "border-b-2 menu-selector pb-1"
+                      : "border-b-2 border-transparent"
+                    }`}
+                  onClick={(e) => handleLinkClickSelect(e, item.id)}
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+            {/* Botão de contato */}
+            <li>
+              <HoverBorderGradient
+                as="button"
+                onClick={(e) => handleLinkClickSelect(e, "#contato")}
+                containerClassName={`transform hover:scale-110 transition-all duration-500 ease-in-out ${selectedSection === "#contato" ? "border-gray-600" : ""
+                  }`}
+                className="contact-button font-semibold"
+              >
+                {t("header.menu.contact")}
+              </HoverBorderGradient>
+            </li>
+            <button
+              onClick={toggleTheme}
+              data-testid="theme-wrapper"
+              className="p-2 rounded-full transition duration-300 flex items-center justify-center w-12 h-12 border-none"
+            >
+              {theme === 'dark' ? <Sun size={22} className="text-yellow-400 hover:scale-110" /> : <Moon size={22} className="text-blue-600 hover:scale-110" />}
+            </button>
+          </ul>
+        </nav>
+      </header>
+
+      {/* Botão para voltar ao topo */}
+      {showScrollButton && (
+        <button
+          onClick={scrollToTop}
+          className="scroll-to-top-button w-12 h-10 fixed bottom-28 mr-[1%] p-2 shadow-lg 
+          rounded-lg transform hover:scale-110 transition duration-300 border border-zinc-400 z-50"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 mx-auto text-white"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M5 15l7-7 7 7" />
+          </svg>
+        </button>
+      )}
+
+    </>
+  );
+};
+
+export default Header;
