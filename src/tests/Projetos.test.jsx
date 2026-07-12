f28cfecd971a6fea79566e44193f0de147b0b15c
vazvdr <azevedo.vanderson@hotmail.com>
2026-07-11 21:40:00 -0300

    Alterando a ordem dos projetos e atualizando o teste para continuar passando

--- src/tests/Projetos.test.jsx
+++ src/tests/Projetos.test.jsx
@@ -20,7 +20,7 @@ describe('Projetos section', () => {
       </I18nextProvider>
     );
 
-    expect(screen.getByText(/GamerStore/i)).toBeInTheDocument();
+    expect(screen.getByText(/Class Scheduling/i)).toBeInTheDocument();
   });
 
   it('deve renderizar um card em telas pequenas', () => {
@@ -33,7 +33,7 @@ describe('Projetos section', () => {
       </I18nextProvider>
     );
 
-    expect(screen.getByText(/GamerStore/i)).toBeInTheDocument();
+    expect(screen.getByText(/Class Scheduling/i)).toBeInTheDocument();
   });
 
   it('deve avançar os cards ao clicar no botão next', () => {
